00389df3e44f5b535dc2e2b4b95d43b9c85a1c42
sudathusjp <usjpsudath@gmail.com>
2021-10-24 22:25:42 +0530

    image Added

--- components/Homedesignlogosection.js
+++ components/Homedesignlogosection.js
@@ -60,55 +60,106 @@ const Homedesignlogosection = () => {
             </h1>
         </section>
         <section>
-            <div className="grid grid-cols-4 gap-0.5 px-40">
-                <div className="industry-card">
-                <img src="/images/industry/1.png"/>
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
-                <div className="industry-card">
-                <img src="/images/industry/1.png" class="img-fluid" />
-                <p class="text-white py-2 text-center">Agriclulture</p>
-                </div>
+            <div className="grid grid-cols-4 gap-4 px-40">
+
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/agriculture.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Agriclulture</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/business.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Business</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/idea.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Creative</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/joystick.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Entertainment</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/finance.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Finance</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/capitol.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Government</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/heartbeat.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Health</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/toys.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Kid</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/manufacturing.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Manufacturing</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/social-media.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Marketing</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/user.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Media</p>
+                </section>
+            </div>
+            <div className="industry-card">
+                <section class="flex justify-center">
+                    <img  src="/images/industry/box.svg" className="h-12" />
+                </section>
+                <section>
+                    <p class="text-white py-2 text-center">Shipping</p>
+                </section>
+            </div>
+            
+            
             </div>
             <p className="text-center font-semibold font-Poppins">Browse All Industries </p>
         </section>
