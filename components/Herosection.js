fc3dcbbc125e68813ab64dcc4cf21eea6ed2dc43
sudathusjp <usjpsudath@gmail.com>
2021-10-29 09:33:56 +0530

    updates done by...

--- components/Herosection.js
+++ components/Herosection.js
@@ -7,32 +7,30 @@ import { PencilIcon } from '@heroicons/react/solid'
 const Herosection = () => {
     return (
         <div className="contain py-10">
-            <section className="card mr-14">
-            <h1 className="text-5xl text-primary-600 font-bold text-left pt-16">
+            <section className="card">
+            <h1 className="text-5xl text-primary-600 font-semibold text-left pt-16">
                 Create Stunning logo designs in few clicks
             </h1>
             <p className="text-gray-400 text-left py-4 font-medium text-lg font-Poppins">
             High resolution logos in just few clicks. No design experience necessary. Logos are built  by our top class designers. You just have to select best from the choices available.
             </p>
 
-            <div className="w-5/5 md:flex sm:flex md:w-5/5">
-            <div className="md:w-3/5 sm:w-3/5 py-1">
-            <div className="flex border-2 hover:shadow-lg focus-within:shadow-lg max-w-full rounded-md px-5 py-3">
-                <input 
-                type="text" 
-                placeholder="Enter your brand name"
-                className="flex-grow focus:outline-none" />
-                <PencilIcon className="h-5 w-5 text-gray-500"/>
-            </div>
-            </div>
-            <div className="md:w-2/5 sm:w-2/5 py-1">
-            <button class="ml-1 w-full bg-primary text-white px-6 py-3 rounded font-Poppins font-semibold ">Genarate Logo</button>
-            </div>
-            </div>
+  <div class="lg:flex md:flex">
+    <div class="relative mb-3 w-full flex flex-wrap items-stretch">
+      <input type="text" class="relative py-3 px-2 pr-10 w-full bg-white rounded shadow outline-none border border-gray-300 text-sm text-gray-700 placeholder-gray-400 focus:outline-none focus:shadow-outline" placeholder="Enter your brand name"  />
+      <span class="absolute right-0 z-10 py-1 pr-2 w-8 h-full leading-snug bg-transparent rounded text-base font-normal text-gray-400 text-center flex items-center justify-center">
+      <PencilIcon className="h-5 w-5 text-gray-500"/>
+      </span>
+    </div>
+    <div class="relative mb-3 w-full flex flex-wrap items-stretch">
+    <button class="ml-1 max-w-sm md:max-w-lg bg-primary text-white px-4 py-2 rounded font-Poppins text-sm font-semibold ">Genarate Logo</button>
+    </div>
+  </div>
+
 
             </section>
 
-            <section className="card">
+            <section className="card slider-card">
             <Carousel autoPlay infiniteLoop showStatus="" showThumbs="" interval="5000" transitionTime="2000">
                 <div>
                     <img src="/images/slider/Eriena logo preview.png" />
